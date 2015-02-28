888d2e7c687d5bd9c5198da42de076ebc6ff04a0
Lauri Hakkarainen <bionik@gmail.com>
2015-03-01 00:35:39 +0200

    Commented the code

--- hackbot.js
+++ hackbot.js
@@ -1,17 +1,21 @@
+//Create configuration object
 var config = {
 	channels: ['#hacklabturku'],
 	server: 'open.ircnet.net',
 	botName: 'hackbot'
 };
 
+//Load libraries
 var http = require('http');
 var irc = require('irc');
 var u = require('underscore');
 
+//Initialize bot object
 var bot = new irc.Client(config.server, config.botName, {
 	channels: config.channels
 });
 
+//Function to perform a http GET request
 function get(url, success, error) {
 	http.get(url, function(res) {
 		body = "";
@@ -26,31 +30,40 @@ function get(url, success, error) {
   });
 }
 
+//Add a listener for incoming message
 bot.addListener('message', function(from, to, text, message) {
-	//console.log(from+' '+to+' '+text+' '+message);
-	//bionik #hacklabturku hackbot [object Object]
+	//from: user, to: channel or nick, text: text, message: object
 
+  //Respond to own name
 	if (text == config.botName){
 		bot.say(from, 'Hi! Write help for available commands.');
 
+  //Respond to "help", if sent directly to me (msg)
 	} else if(to == config.botName && text == 'help') {
     bot.say(from, 'For now, you can use the following commands:');
     bot.say(from, '!hacklab - Displays current status of the lab');
 
+  //Respond to "!hacklab"
   } else if (text == '!hacklab'){
-		//Check lights
 		var error = false;
-		var body;
 
+    //Init variables
     var room1;
     var room2;
     var temp;
 
+    //Wait until finished is called 3 times
 		var finished = u.after(3, function(){
+
+      //If no errors...
       if(!error){
+        //Init output text
         var text = '';
+
+        //Format & round temperature text
         var temp_text = 'Temperature is '+(Math.round(temp.data*10)/10)+'°C';
 
+        //Output logic...
         if(room1.data == 1 && room2.data == 1){
           text = 'Lights are off. Hacklab is probably empty. '+temp_text;
         } else if (room1.data == 1 && room2.data == 0){
@@ -61,6 +74,7 @@ bot.addListener('message', function(from, to, text, message) {
           text = 'Lights are on in both rooms! '+temp_text;
         }
 
+        //Send to channel or nick?
         if (to == config.botName) {
           bot.say(from, text);
         } else {
@@ -68,12 +82,17 @@ bot.addListener('message', function(from, to, text, message) {
         }
 
       } else {
+        //Print error
         console.log('ERROR: Could not fetch data!');
         bot.say(from, 'ERROR: Could not fetch data! Sorry :(');
       }
 
 		});
 
+    //The following get requests call finished. When it's called three times,
+    //we know the queries are complete and can start outputting.
+
+    //Fetch room 1
     get('http://localhost/gpio/?a=readPin&pin=0', function(response){
       console.log(response);
       room1 = response;
@@ -83,6 +102,7 @@ bot.addListener('message', function(from, to, text, message) {
       finished();
     });
 
+    //Fetch room 2
     get('http://localhost/gpio/?a=readPin&pin=1', function(response){
       console.log(response);
       room2 = response;
@@ -92,6 +112,7 @@ bot.addListener('message', function(from, to, text, message) {
       finished();
     });
 
+    //Fetch temperature data
     get('http://localhost/temp/?a=getTemp', function(response){
       console.log(response);
       temp = response;
@@ -105,6 +126,7 @@ bot.addListener('message', function(from, to, text, message) {
 
 });
 
+//Catch errors
 bot.addListener('error', function(message) {
     console.log('ERROR: ', message);
 });
